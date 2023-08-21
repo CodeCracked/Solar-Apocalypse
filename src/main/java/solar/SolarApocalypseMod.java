c59e5f63307c3951cabd67df768a8f5a01cf030b
Markus Snodgrass <codecrackedgames@gmail.com>
2023-08-20 20:37:39 -0700

    Initial Commit
    * Initial commit containing my Fabric mod template, updated to 1.20.1

---
+++ src/main/java/solar/SolarApocalypseMod.java
@@ -0,0 +1,44 @@
+package solar;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.random.Random;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import solar.utils.VersionChecker;
+
+public class SolarApocalypseMod implements ModInitializer, ClientModInitializer
+{
+    public static final String MODID = "solar";
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Random RANDOM = Random.createThreadSafe();
+
+    private boolean ranVersionCheck;
+    
+    @Override
+    public void onInitialize()
+    {
+    
+    }
+    @Override
+    public void onInitializeClient()
+    {
+        ClientEntityEvents.ENTITY_LOAD.register(this::onWorldLoaded);
+    }
+    
+    private void onWorldLoaded(Entity entity, ClientWorld world)
+    {
+        if (entity instanceof PlayerEntity)
+        {
+            if (!ranVersionCheck)
+            {
+                ranVersionCheck = true;
+                VersionChecker.doVersionCheck();
+            }
+        }
+    }
+}
